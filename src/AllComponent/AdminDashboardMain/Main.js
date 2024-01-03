8071f526a1f2f3e649cf725ef5958cc5568fd99e
Bittu singh <bittusingh8991@gmail.com>
2024-01-03 16:47:53 +0530

    pull

--- src/AllComponent/AdminDashboardMain/Main.js
+++ src/AllComponent/AdminDashboardMain/Main.js
@@ -1,12 +1,76 @@
-import React from "react";
-import Dashboard from "../DAshBoardRight/Dashboard";
+// import React from "react";
+// import Dashboard from "../DAshBoardRight/Dashboard";
 
-function Main() {
+// function Main() {
+//   return (
+//     <main className="main-container">
+//       <Dashboard />
+//     </main>
+//   );
+// }
+
+// export default Main;
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+
+import Box from '@mui/material/Box';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+export default function Main() {
+
   return (
-    <main className="main-container">
-      <Dashboard />
-    </main>
+    // <Box sx={{ flexGrow: 1 }}>
+     
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+            //  </Box>
   );
 }
-
-export default Main;
